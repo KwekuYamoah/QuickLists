81ebf5305c2dc92b4db606640263a46fac162edf
KwekuYamoah <kwekuandyamoah@gmail.com>
2020-08-03 23:42:32 +0000

    Lesson four done(Service and Interface)

--- src/app/services/checklist-data.service.ts
+++ src/app/services/checklist-data.service.ts
@@ -15,32 +15,90 @@ export class ChecklistDataService {
     return Promise.resolve(true);
   }
 
-  createChecklist(data): void {}
+  createChecklist(data): void {
 
-  renameChecklist(checklist, data): void {}
-
-  removeChecklist(checklist): void {}
-
-  getChecklist(id): Checklist{
-    return{
-      id: '',
-      title: '',
+    this.checklists.push({
+      id: this.generateSlug(data.name),
+      title: data.name,
       items: []
+    });
 
-    };
+    this.save();
   }
 
-  addItem(checklistId, data): void {}
+  renameChecklist(checklist, data): void {
 
-  removeItem(checklist, item): void {}
+    let index = this.checklists.indexOf(checklist);
 
-  renameItem(item, data): void {}
+    if (index > -1){
+      this.checklists[index].title = data.name;
+      this.save();
+    }
+  }
 
-  toggleItem(item): void {}
+  removeChecklist(checklist): void {
+    let index = this.checklists.indexOf(checklist);
+
+    if (index > -1){
+      this.checklists.splice(index,1);
+      this.save();
+    }
+  }
+
+  getChecklist(id): Checklist{
+    return this.checklists.find(checklist => checklist.id === id);
+  }
+
+  addItem(checklistId, data): void {
+    /*adds check list item*/
+
+    this.getChecklist(checklistId).items.push({
+      title: data.name,
+      checked: false
+    });
+    this.save();
+  }
+
+  removeItem(checklist, item): void {
+    /* Removes a ChecklistItem */
+    let index = checklist.items.indexOf(item);
+
+    if (index > -1){
+      checklist.items.splice(index, 1);
+      this.save();
+    }
+
+  }
+
+  renameItem(item, data): void {
+
+    item.title = data.name;
+    this.save();
+
+  }
+
+  toggleItem(item): void {
+    /*flips the value of a particular item and saves it to storage*/
+
+    item.checked = !item.checked;
+    this.save();
+  }
 
   save(): void {}
-  
+
   generateSlug(title): string {
-    return '';
+
+    // Note: This is a simplistic slug generator and will not handle things like special characters
+
+    let slug = title.toLowerCase().replace(/\s+/g, '-');
+    // Check if slug already exists
+    let exists = this.checklists.filter((checklist) => {
+      return checklist.id.substring(0, slug.length) === slug;
+    });
+    // if the title is already being used, add a number to make the slug unique
+    if (exists.length > 0){
+      slug = slug + exists.length.toString();
+    }
+    return slug;
   }
 }
