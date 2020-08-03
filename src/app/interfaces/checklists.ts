82f2f35cee9b66e341f6b0074f8907afe5da5be0
KwekuYamoah <kwekuandyamoah@gmail.com>
2020-08-03 22:59:15 +0000

    hompage and checklist page done. interface

---
+++ src/app/interfaces/checklists.ts
@@ -0,0 +1,11 @@
+/*Creating custom Interface*/
+export interface Checklist {
+    id: string;
+    title: string;
+    items: ChecklistItem[];
+}
+
+export interface ChecklistItem {
+    title: string;
+    checked: boolean;
+}
--- src/app/services/checklist-data.service.ts
+++ src/app/services/checklist-data.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Checklist } from '../interfaces/checklists';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChecklistDataService {
+  public checklists: Checklist[] = [];
+  public loaded = false;
+
 
   constructor() { }
+
+  load(): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+
+  createChecklist(data): void {}
+
+  renameChecklist(checklist, data): void {}
+
+  removeChecklist(checklist): void {}
+
+  getChecklist(id): Checklist{
+    return{
+      id: '',
+      title: '',
+      items: []
+
+    };
+  }
+
+  addItem(checklistId, data): void {}
+
+  removeItem(checklist, item): void {}
+
+  renameItem(item, data): void {}
+
+  toggleItem(item): void {}
+
+  save(): void {}
+  
+  generateSlug(title): string {
+    return '';
+  }
 }
